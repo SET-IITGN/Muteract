bf31fa5e6e60b06116bc2460616a90439a64acff
Mukul Paras Potta <50733013+MukulParasPotta@users.noreply.github.com>
2025-05-05 10:07:31 +0000

    v0.1.4: Added warning when NLTK_DATA is not set

--- src/Muteract/main.py
+++ src/Muteract/main.py
@@ -1,10 +1,13 @@
 import os
 import pathlib
+import sys
 import webbrowser
 
 def start():
     if not os.environ.get('OPEN_AI_API_KEY', ""):
         raise Exception("API Key not configured!!\n\nSet the OPEN_AI_API_KEY environment variable to a valid API key to start the application!!!\nExiting...")
+    if not os.environ('NLTK_DATA', ""):
+        print("Path to NLTK Data not found!! Comparisons CANNOT be performed using the Comparison button!!", file=sys.stderr)
     os.system(f'python {pathlib.Path(__file__).parent.resolve()}/manage.py runserver localhost:8000 > muteract_runlog.log 2>&1 &')
     webbrowser.open("localhost:8000")
 
