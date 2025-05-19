2d76698b00e7dc689497e70f19edf3868ca428e8
Mukul Paras Potta <50733013+MukulParasPotta@users.noreply.github.com>
2025-05-19 16:16:27 +0000

    v0.1.4: Updated code to run without opening a browser window

--- src/Muteract/main.py
+++ src/Muteract/main.py
@@ -2,14 +2,18 @@ import os
 import pathlib
 import sys
 import webbrowser
+import argparse
 
-def start():
+if __name__ == "__main__":
     if not os.environ.get('OPEN_AI_API_KEY', ""):
         raise Exception("API Key not configured!!\n\nSet the OPEN_AI_API_KEY environment variable to a valid API key to start the application!!!\nExiting...")
     if not os.environ('NLTK_DATA', ""):
         print("Path to NLTK Data not found!! Comparisons CANNOT be performed using the Comparison button!!", file=sys.stderr)
+    parser = argparse.ArgumentParser(description="Muteract")
+    parser.add_argument('--silent', action='store_true', help='Run in silent mode (does not open a browser window)')
+    args = parser.parse_args()
     os.system(f'python {pathlib.Path(__file__).parent.resolve()}/manage.py runserver localhost:8000 > muteract_runlog.log 2>&1 &')
-    webbrowser.open("localhost:8000")
-
-if __name__ == "__main__":
-    start()
+    if not args.silent:
+        webbrowser.open("localhost:8000")
+    else:
+        print("Starting in Silent mode...")
